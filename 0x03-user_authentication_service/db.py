7bbdf58df9e0a27793ce1af5ea9fd3b5ae1f68c5
Xage1 <kennethkiarie555@gmail.com>
2024-04-24 07:02:35 +0000

    This is awesome

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
 
+
 class DB:
     """DB class
     """
@@ -37,4 +38,3 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
