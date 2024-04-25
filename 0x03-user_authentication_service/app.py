a59cf1940012f3a763e2ea74e26ffdd02c24e52d
Xage1 <kennethkiarie555@gmail.com>
2024-04-25 12:11:36 +0000

    This is PYTHON

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from auth import Auth
 
 
@@ -9,27 +9,27 @@ AUTH = Auth()
 
 
 @app.route("/")
-def welcome():
+def welcome() -> str:
     """
     Route to return a JSON payload
     """
 
     return jsonify({"message": "Bienvenue"})
 
-@app.route("/users", methods=["POST"])
-def register_user():
-    """
-    Endpoint to register a user
-    """
-    email = request.form.get("email")
-    password = request.form.get("password")
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user() -> str:
+    """Endpoint to register a user"""
 
+    email, password = request.form.get("email"), request.form.get("password")
     try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": email, "message" : "user created"}), 200
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError as error:
+        if "already exists" in str(error):
+            return jsonify({"message": "email already registered"}), 400
+    else:
+        return jsonify({"message": str(error)}), 500
 
 
 if __name__ == "__main__":
