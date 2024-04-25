c19afeb782ae501b1f5385de22483ad8184714f3
Xage1 <kennethkiarie555@gmail.com>
2024-04-25 11:27:10 +0000

    This is PYTHON

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,12 +4,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def welcome():
     """
     Route to return a JSON payload
     """
+
     return jsonify({"message": "Bienvenue"})
 
+
 if __name__ == "__main__":
+
     app.run(host="0.0.0.0", port="5000")
