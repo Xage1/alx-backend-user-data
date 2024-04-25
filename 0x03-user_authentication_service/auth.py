11f88991e19dfe4d00be296cb1e9e42e6f0e4e36
Xage1 <kennethkiarie555@gmail.com>
2024-04-25 07:47:10 +0000

    Almost there

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Hashed password Task
+"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt
+    """
+    salt = bcrypt.gensalt()
+
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    return hashed_password
