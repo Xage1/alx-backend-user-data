8b0a66d034f83dfcdb98173b0d1f4e701d6da814
Xage1 <kennethkiarie555@gmail.com>
2024-04-25 08:32:48 +0000

    Almost there

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,26 +19,28 @@ def _hash_password(password: str) -> bytes:
 
     return hashed_password
 
+
 class Auth:
     """
     Auth class to interact with the authentication database
     """
-
     def __init__(self):
         self._db = DB()
-
 
     def register_user(self, email: str, password: str) -> User:
         """
         Registers a new User
         """
 
-        try :
+        try:
             existing_user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except ValueError:
 
             hashed_password = _hash_password(password)
 
-            new_user = self._db.add_user(email=email, hashed_password=hashed_password)
+            new_user = self._db.add_user(
+                    email=email,
+                    hashed_password=hashed_password
+            )
             return new_user
